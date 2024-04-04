66264cd350b322fef6c7c1577204fd2d61198c57
sookim-1 <scstnghks@gmail.com>
2024-04-04 15:09:22 +0900

    [Project] : Xcode Project 추가

---
+++ GeekReport/GeekReport/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  GeekReport
+//
+//  Created by sookim on 4/4/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+    }
+
+}
