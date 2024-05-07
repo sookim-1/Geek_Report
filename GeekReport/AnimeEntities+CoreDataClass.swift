46031e6f321d5c7a0cd6c270ed3569c64cf04b8f
sookim-1 <scstnghks@gmail.com>
2024-05-07 11:25:57 +0900

    [Feat] : 시청 중에피소드 저장기능 구현

---
+++ GeekReport/AnimeEntities+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  AnimeEntities+CoreDataClass.swift
+//  GeekReport
+//
+//  Created by sookim on 5/7/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(AnimeEntities)
+public class AnimeEntities: NSManagedObject {
+
+}
